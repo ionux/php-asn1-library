e411f5cf5fa442aa0326f1db516617feb3d1922b
Rich Morgan <ionux@users.noreply.github.com>
2016-06-07 10:01:24 -0400

    Create bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * These tests are a work in progress. If you have ideas
+ * for additional or improved test cases, please submit
+ * a pull request.
+ *
+ * Thanks,
+ * Rich <rich@richmorgan.me>
+ */
+ 
+require 'vendor/autoload.php';
