19334ea0f960adaf7a812b68780490f79c7523fa
Rich Morgan <ionux@users.noreply.github.com>
2016-06-07 11:16:40 -0400

    Added my standard file comment header block

--- src/Pem.php
+++ src/Pem.php
@@ -1,17 +1,37 @@
 <?php
-/**
- *  Parses a PEM (base-64 encoded DER) file
+/******************************************************************************
+ * This file is part of the PHP ASN.1 Library project. You can always find the
+ * latest version of this class and project at: https://github.com/ionux/php-asn1-library
  *
- *  (c) 2014-2016, Rich Morgan <rich@richmorgan.me>
- * 
- *  This code is released under the MIT License (MIT).
- *  See the LICENSE file for the complete text or 
- *  notify me if you did not receive a copy with this
- *  file.
- */
+ * Copyright (c) 2014-2016 Rich Morgan, rich@richmorgan.me
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
 
 namespace ASN1;
 
+/**
+ * Parses, decodes and encodes a PEM (base-64 encoded DER) file or data string.
+ *
+ * @author Rich Morgan <rich@richmorgan.me>
+ */
 final class Pem
 {
     /**
