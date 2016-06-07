184268c4b90533ca32b0cdb61a4e40c070a76457
Rich Morgan <ionux@users.noreply.github.com>
2016-06-07 13:40:28 -0400

    Fixed conflicting constant names

--- src/Pem.php
+++ src/Pem.php
@@ -65,12 +65,12 @@ final class Pem
      *     Context-specific - The meaning of this type depends on the context
      *     Private          - This type is defined in a private specification
      *
-     *     Class Name         Hex Value      Bit 8  Bit 7
+     *     Class Name            Hex Value      Bit 8  Bit 7
      * ---------------------------------------------------------------------- */
-    const  UNIVERSAL         = '0x00';    //   0      0 
-    const  APPLICATION       = '0x40';    //   0      1
-    const  CONTEXT           = '0x80';    //   1      0
-    const  PRIVATE           = '0xC0';    //   1      1
+    const  UNIVERSAL_CB         = '0x00';    //   0      0 
+    const  APPLICATION_CB       = '0x40';    //   0      1
+    const  CONTEXT_CB           = '0x80';    //   1      0
+    const  PRIVATE_SPEC_CB      = '0xC0';    //   1      1
 
     /**
      * Primitive/Constructed content type bits
@@ -78,10 +78,10 @@ final class Pem
      * Bit 6 (P/C) states whether the content is primitive, like an INTEGER, or
      * constructed, which means it holds further TLV values, like a SET.
      *
-     *     Content Type       Hex Value     Bit 6
+     *     Content Type          Hex Value     Bit 6
      * ---------------------------------------------------------------------- */
-    const  PRIMITIVE         = '0x00';    //   0
-    const  CONSTRUCTED       = '0x20';    //   1
+    const  PRIMITIVE_CT         = '0x00';    //   0
+    const  CONSTRUCTED_CT       = '0x20';    //   1
 
     /**
      * Universal Class Tags
@@ -89,48 +89,48 @@ final class Pem
      * The remaining bits 5 to 1 contain the tag, which serves as the identifier
      * of the type of the content. The following tags are native to ASN.1:
      *
-     *     Tag Name           Hex Value      P/C    Dec Value
+     *     Tag Name               Hex Value      P/C    Dec Value
      * ---------------------------------------------------------------------- */
-    const  END_OF_CONTENT    = '0x00';    //  P         0 
-    const  BOOLEAN           = '0x01';    //  P         1
-    const  INTEGER           = '0x02';    //  P         2
-    const  BIT_STRING        = '0x03';    //  P/C       3
-    const  OCTET_STRING      = '0x04';    //  P/C       4 
-    const  NULL              = '0x05';    //  P         5
-    const  OBJ_IDENTIFIER    = '0x06';    //  P         6 
-    const  OBJ_DESCRIPTOR    = '0x07';    //  P/C       7 
-    const  EXTERNAL          = '0x08';    //  C         8 
-    const  FLOAT             = '0x09';    //  P         9 
-    const  ENUMERATED        = '0x0A';    //  P        10 
-    const  EMBEDDED_PDV      = '0x0B';    //  C        11 
-    const  UTF8_STRING       = '0x0C';    //  P/C      12
-    const  RELATIVE_OID      = '0x0D';    //  P        13
-    const  RESERVED_A        = '0x0E';    //  -        14
-    const  RESERVED_B        = '0x0F';    //  -        15
-    const  SEQUENCE          = '0x10';    //  C        16
-    const  SET               = '0x11';    //  C        17
-    const  NUMERIC_STRING    = '0x12';    //  P/C      18
-    const  PRINTABLE_STRING  = '0x13';    //  P/C      19
-    const  T61_STRING        = '0x14';    //  P/C      20
-    const  VIDEOTEX_STRING   = '0x15';    //  P/C      21
-    const  IA5_STRING        = '0x16';    //  P/C      22
-    const  UTC_TIME          = '0x17';    //  P/C      23
-    const  GENERALIZED_TIME  = '0x18';    //  P/C      24
-    const  GRAPHIC_STRING    = '0x19';    //  P/C      25
-    const  VISIBLE_STRING    = '0x1A';    //  P/C      26
-    const  GENERAL_STRING    = '0x1B';    //  P/C      27
-    const  UNIVERSAL_STRING  = '0x1C';    //  P/C      28
-    const  CHARACTER_STRING  = '0x1D';    //  P/C      29
-    const  BMPSTRING         = '0x1E';    //  P/C      30
-    const  USE_LONG_FORM     = '0x1F';    //  -        31
+    const  END_OF_CONTENT_TAG    = '0x00';    //  P         0 
+    const  BOOLEAN_TAG           = '0x01';    //  P         1
+    const  INTEGER_TAG           = '0x02';    //  P         2
+    const  BIT_STRING_TAG        = '0x03';    //  P/C       3
+    const  OCTET_STRING_TAG      = '0x04';    //  P/C       4 
+    const  NULL_TAG              = '0x05';    //  P         5
+    const  OBJ_IDENTIFIER_TAG    = '0x06';    //  P         6 
+    const  OBJ_DESCRIPTOR_TAG    = '0x07';    //  P/C       7 
+    const  EXTERNAL_TAG          = '0x08';    //  C         8 
+    const  FLOAT_TAG             = '0x09';    //  P         9 
+    const  ENUMERATED_TAG        = '0x0A';    //  P        10 
+    const  EMBEDDED_PDV_TAG      = '0x0B';    //  C        11 
+    const  UTF8_STRING_TAG       = '0x0C';    //  P/C      12
+    const  RELATIVE_OID_TAG      = '0x0D';    //  P        13
+    const  RESERVED_A_TAG        = '0x0E';    //  -        14
+    const  RESERVED_B_TAG        = '0x0F';    //  -        15
+    const  SEQUENCE_TAG          = '0x10';    //  C        16
+    const  SET_TAG               = '0x11';    //  C        17
+    const  NUMERIC_STRING_TAG    = '0x12';    //  P/C      18
+    const  PRINTABLE_STRING_TAG  = '0x13';    //  P/C      19
+    const  T61_STRING_TAG        = '0x14';    //  P/C      20
+    const  VIDEOTEX_STRING_TAG   = '0x15';    //  P/C      21
+    const  IA5_STRING_TAG        = '0x16';    //  P/C      22
+    const  UTC_TIME_TAG          = '0x17';    //  P/C      23
+    const  GENERALIZED_TIME_TAG  = '0x18';    //  P/C      24
+    const  GRAPHIC_STRING_TAG    = '0x19';    //  P/C      25
+    const  VISIBLE_STRING_TAG    = '0x1A';    //  P/C      26
+    const  GENERAL_STRING_TAG    = '0x1B';    //  P/C      27
+    const  UNIVERSAL_STRING_TAG  = '0x1C';    //  P/C      28
+    const  CHARACTER_STRING_TAG  = '0x1D';    //  P/C      29
+    const  BMPSTRING_TAG         = '0x1E';    //  P/C      30
+    const  USE_LONG_FORM_TAG     = '0x1F';    //  -        31
 
     /**
      * Identifer tags greater than 30
      *
      * See: http://en.wikipedia.org/wiki/X.690
      * ---------------------------------------------------------------------- */
-    const  LEN_INDEF         = '0x80';    // Indefinite, or Long Form 
-    const  LEN_SHORT         = '0x7F';    // Definite, or Short Form 
+    const  LEN_INDEF_TAG         = '0x80';    // Indefinite, or Long Form 
+    const  LEN_SHORT_TAG         = '0x7F';    // Definite, or Short Form 
 
     /**
      * Public constructor method.
