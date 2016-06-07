0ef026001ea20fd6b545a61c2caef99e27b4cf6f
Rich Morgan <ionux@users.noreply.github.com>
2016-06-07 11:12:15 -0400

    Fix for missing semicolons
    And corrected scope on pemDecode() method.  Some other minor refactoring & formatting, as well.

--- src/Pem.php
+++ src/Pem.php
@@ -27,9 +27,9 @@ final class Pem
      *
      *     Octet Mask Name    Hex Value
      * ---------------------------------------------------------------------- */
-    const  CLASS_BITMASK     = '0xC0'
-    const  PC_BITMASK        = '0x20'
-    const  TAGNUM_BITMASK    = '0x1F'
+    const  CLASS_BITMASK     = '0xC0';
+    const  PC_BITMASK        = '0x20';
+    const  TAGNUM_BITMASK    = '0x1F';
     
     /**
      * Class bits in a Type identifier octet
@@ -45,10 +45,10 @@ final class Pem
      *
      *     Class Name         Hex Value      Bit 8  Bit 7
      * ---------------------------------------------------------------------- */
-    const  UNIVERSAL         = '0x00'    //   0      0 
-    const  APPLICATION       = '0x40'    //   0      1
-    const  CONTEXT           = '0x80'    //   1      0
-    const  PRIVATE           = '0xC0'    //   1      1
+    const  UNIVERSAL         = '0x00';    //   0      0 
+    const  APPLICATION       = '0x40';    //   0      1
+    const  CONTEXT           = '0x80';    //   1      0
+    const  PRIVATE           = '0xC0';    //   1      1
 
     /**
      * Primitive/Constructed content type bits
@@ -58,8 +58,8 @@ final class Pem
      *
      *     Content Type       Hex Value     Bit 6
      * ---------------------------------------------------------------------- */
-    const  PRIMITIVE         = '0x00'    //   0
-    const  CONSTRUCTED       = '0x20'    //   1
+    const  PRIMITIVE         = '0x00';    //   0
+    const  CONSTRUCTED       = '0x20';    //   1
 
     /**
      * Universal Class Tags
@@ -69,44 +69,50 @@ final class Pem
      *
      *     Tag Name           Hex Value      P/C    Dec Value
      * ---------------------------------------------------------------------- */
-    const  END_OF_CONTENT    = '0x00'    //   P         0 
-    const  BOOLEAN           = '0x01'    //   P         1
-    const  INTEGER           = '0x02'    //   P         2
-    const  BIT_STRING        = '0x03'    //   P/C       3
-    const  OCTET_STRING      = '0x04'    //   P/C       4 
-    const  NULL              = '0x05'    //   P         5
-    const  OBJ_IDENTIFIER    = '0x06'    //   P         6 
-    const  OBJ_DESCRIPTOR    = '0x07'    //   P/C       7 
-    const  EXTERNAL          = '0x08'    //   C         8 
-    const  FLOAT             = '0x09'    //   P         9 
-    const  ENUMERATED        = '0x0A'    //   P        10 
-    const  EMBEDDED_PDV      = '0x0B'    //   C        11 
-    const  UTF8_STRING       = '0x0C'    //   P/C      12
-    const  RELATIVE_OID      = '0x0D'    //   P        13
-    const  RESERVED_A        = '0x0E'    //   -        14
-    const  RESERVED_B        = '0x0F'    //   -        15
-    const  SEQUENCE          = '0x10'    //   C        16
-    const  SET               = '0x11'    //   C        17
-    const  NUMERIC_STRING    = '0x12'    //   P/C      18
-    const  PRINTABLE_STRING  = '0x13'    //   P/C      19
-    const  T61_STRING        = '0x14'    //   P/C      20
-    const  VIDEOTEX_STRING   = '0x15'    //   P/C      21
-    const  IA5_STRING        = '0x16'    //   P/C      22
-    const  UTC_TIME          = '0x17'    //   P/C      23
-    const  GENERALIZED_TIME  = '0x18'    //   P/C      24
-    const  GRAPHIC_STRING    = '0x19'    //   P/C      25
-    const  VISIBLE_STRING    = '0x1A'    //   P/C      26
-    const  GENERAL_STRING    = '0x1B'    //   P/C      27
-    const  UNIVERSAL_STRING  = '0x1C'    //   P/C      28
-    const  CHARACTER_STRING  = '0x1D'    //   P/C      29
-    const  BMPSTRING         = '0x1E'    //   P/C      30
-    const  USE_LONG_FORM     = '0x1F'    //   -        31
+    const  END_OF_CONTENT    = '0x00';    //  P         0 
+    const  BOOLEAN           = '0x01';    //  P         1
+    const  INTEGER           = '0x02';    //  P         2
+    const  BIT_STRING        = '0x03';    //  P/C       3
+    const  OCTET_STRING      = '0x04';    //  P/C       4 
+    const  NULL              = '0x05';    //  P         5
+    const  OBJ_IDENTIFIER    = '0x06';    //  P         6 
+    const  OBJ_DESCRIPTOR    = '0x07';    //  P/C       7 
+    const  EXTERNAL          = '0x08';    //  C         8 
+    const  FLOAT             = '0x09';    //  P         9 
+    const  ENUMERATED        = '0x0A';    //  P        10 
+    const  EMBEDDED_PDV      = '0x0B';    //  C        11 
+    const  UTF8_STRING       = '0x0C';    //  P/C      12
+    const  RELATIVE_OID      = '0x0D';    //  P        13
+    const  RESERVED_A        = '0x0E';    //  -        14
+    const  RESERVED_B        = '0x0F';    //  -        15
+    const  SEQUENCE          = '0x10';    //  C        16
+    const  SET               = '0x11';    //  C        17
+    const  NUMERIC_STRING    = '0x12';    //  P/C      18
+    const  PRINTABLE_STRING  = '0x13';    //  P/C      19
+    const  T61_STRING        = '0x14';    //  P/C      20
+    const  VIDEOTEX_STRING   = '0x15';    //  P/C      21
+    const  IA5_STRING        = '0x16';    //  P/C      22
+    const  UTC_TIME          = '0x17';    //  P/C      23
+    const  GENERALIZED_TIME  = '0x18';    //  P/C      24
+    const  GRAPHIC_STRING    = '0x19';    //  P/C      25
+    const  VISIBLE_STRING    = '0x1A';    //  P/C      26
+    const  GENERAL_STRING    = '0x1B';    //  P/C      27
+    const  UNIVERSAL_STRING  = '0x1C';    //  P/C      28
+    const  CHARACTER_STRING  = '0x1D';    //  P/C      29
+    const  BMPSTRING         = '0x1E';    //  P/C      30
+    const  USE_LONG_FORM     = '0x1F';    //  -        31
 
-    // Identifer tags greater than 30
-    // See: http://en.wikipedia.org/wiki/X.690
-    const  LEN_INDEF         = '0x80'    // Indefinite, or Long Form 
-    const  LEN_SHORT         = '0x7F'    // Definite, or Short Form 
+    /**
+     * Identifer tags greater than 30
+     *
+     * See: http://en.wikipedia.org/wiki/X.690
+     * ---------------------------------------------------------------------- */
+    const  LEN_INDEF         = '0x80';    // Indefinite, or Long Form 
+    const  LEN_SHORT         = '0x7F';    // Definite, or Short Form 
 
+    /**
+     * Public constructor method.
+     */
     public function __construct()
     {
         $this->checkExtensions();
@@ -119,7 +125,7 @@ final class Pem
      * @return array            The keypair info.
      * @throws \Exception
      */
-    private function pemDecode($pem_data = null)
+    public function pemDecode($pem_data = null)
     {
         if (empty($pem_data)) {
             throw new \Exception('Invalid or corrupt secp256k1 key provided. Cannot decode the supplied PEM data.');
@@ -159,7 +165,10 @@ final class Pem
             throw new \Exception('Invalid or corrupt secp256k1 key provided. Cannot decode the supplied PEM data.');
         }
 
-        return array('private_key' => $private_key, 'public_key' => $public_key);
+        return array(
+                     'private_key' => $private_key,
+                     'public_key'  => $public_key
+               );
     }
 
     /**
@@ -211,8 +220,7 @@ final class Pem
 
     	$beg_ec_text = '-----BEGIN EC PRIVATE KEY-----';
     	$end_ec_text = '-----END EC PRIVATE KEY-----';
-
-    	$dec = trim(implode($ecpemstruct));
+    	$dec         = trim(implode($ecpemstruct));
 
     	if (strlen($dec) < 230) {
     		throw new \Exception('Invalid or corrupt secp256k1 keypair provided. Cannot encode the supplied data.');
@@ -227,9 +235,7 @@ final class Pem
     		$byte = $byte . $digits[$rem];
     	}
 
-    	$byte = $beg_ec_text . "\r\n" . chunk_splt(base64_encode(strrev($byte)), 64) . $end_ec_text;
-
-    	return $byte;
+    	return $beg_ec_text . "\r\n" . chunk_splt(base64_encode(strrev($byte)), 64) . $end_ec_text;
     }
 
     /**
