76c1261259c74103cfdd5492907dac2747a59067
Rich Morgan <ionux@users.noreply.github.com>
2025-01-07 13:56:15 -0600

    Updated copyright year.

--- src/Pem.php
+++ src/Pem.php
@@ -3,7 +3,7 @@
  * This file is part of the PHP ASN.1 Library project. You can always find the
  * latest version of this class and project at: https://github.com/ionux/php-asn1-library
  *
- * Copyright (c) 2014-2024 Rich Morgan, rich@richmorgan.me
+ * Copyright (c) 2014-2025 Rich Morgan
  *
  * The MIT License (MIT)
  *
@@ -30,7 +30,7 @@ namespace ASN1;
 /**
  * Parses, decodes and encodes a PEM (base-64 encoded DER) file or data string.
  *
- * @author Rich Morgan <rich@richmorgan.me>
+ * @author Rich Morgan
  */
 final class Pem
 {
